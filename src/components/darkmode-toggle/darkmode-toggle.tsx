9fdcc9b3f438f88be1d2644a665f942a04d04cfe
MarvinMichel <mmhsernee@gmail.com>
2022-04-15 16:25:50 +0200

    [Added] prefers-color-scheme support

--- src/components/darkmode-toggle/darkmode-toggle.tsx
+++ src/components/darkmode-toggle/darkmode-toggle.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h, State, Listen, Host, Watch } from '@stencil/core';
+import { Component, Prop, h, Listen, Host, Watch } from '@stencil/core';
 
 @Component({
   tag: 'eg-darkmode-toggle',
@@ -6,28 +6,32 @@ import { Component, Prop, h, State, Listen, Host, Watch } from '@stencil/core';
   shadow: true,
 })
 export class DarkmodeToggle {
-  @Prop({ mutable: true, reflect: true })
-  active: boolean;
-
   @Prop()
   label: string = 'Darkmode theme';
 
-  @State()
-  activated: boolean;
+  @Prop({ mutable: true, reflect: true })
+  active: boolean;
 
-  @Watch('activated')
+  @Watch('active')
   watchStateHandler(newValue: boolean) {
-    this.active = newValue;
+    document.documentElement.setAttribute('data-theme', newValue ? 'dark' : 'light');
   }
 
   @Listen('click')
-  toggleTheme() {
-    this.activated = !this.activated;
-    document.documentElement.setAttribute('data-theme', this.activated ? 'dark' : 'light');
+  toggleActive() {
+    this.active = !this.active;
   }
 
   componentWillLoad() {
-    this.active = false;
+    const savedTheme = JSON.parse(localStorage.getItem('darkmode'));
+    const prefersDarkmode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (savedTheme) {
+      this.active = savedTheme;
+      return;
+    }
+
+    this.active = prefersDarkmode;
   }
 
   render() {
@@ -38,7 +42,7 @@ export class DarkmodeToggle {
             {this.label}
           </span>
           <span id="toggle" part="toggle" aria-hidden="true">
-            {!this.activated ? (
+            {!this.active ? (
               <slot key="a" name="iconIdle">
                 <img src="/images/sun.svg" alt="" role="icon" />
               </slot>
